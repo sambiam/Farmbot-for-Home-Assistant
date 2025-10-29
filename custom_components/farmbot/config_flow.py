894036f1277afedca2d83012ae5bc58c10fed22d
sambiam <96670272+sambiam@users.noreply.github.com>
2025-10-29 17:07:34 +1030

    Update and rename config_flow.py to custom_components/farmbot/config_flow.py

--- custom_components/farmbot/config_flow.py
+++ custom_components/farmbot/config_flow.py
@@ -83,3 +83,4 @@ class FarmbotConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return self.async_show_form(
             step_id="user", data_schema=data_schema, errors=errors
         )
+
